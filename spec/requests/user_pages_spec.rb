3c99e3084c87612f0021c3275040a77f0956872a
Alex Aur <alexander.aur@gmail.com>
2012-06-12 15:13:37 -0500

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -49,11 +49,15 @@ describe "User pages" do
 			end
 
 			describe "after saving the user" do
+
 				before { click_button submit }
 				let(:user) {User.find_by_email('user@example.com')}
 
 				it { should have_selector('title', text: user.name)}
 				it { should have_selector('div.alert.alert-success', text: 'Welcome')}
+
+				it { should have_link('Sign out')}
+
 			end
 		end
 	end	
